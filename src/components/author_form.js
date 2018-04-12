3d5877b8c4e2c4ac6eefd1968f119c95d1e9352b
Jonathan Engelsma <jengelsma@gmail.com>
2018-04-12 18:56:05 -0400

    added axios

--- src/components/author_form.js
+++ src/components/author_form.js
@@ -1,11 +1,94 @@
 import React from 'react';
 
-const AuthorForm = (props) => {
-  return (
-    <div className="author-form">
-      Our Author Form Goes Here.
-    </div>
-  );
+class AuthorForm extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      fname: "",
+      lname: "",
+      email:  "",
+      id: 0
+    };
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleCancel = this.handleCancel.bind(this);
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleSubmit(event)
+  {
+    this.props.onSubmit({
+      fname: this.state.fname,
+      lname: this.state.lname,
+      email: this.state.email,
+      id: this.state.id,
+    });
+    event.preventDefault();
+  }
+
+  handleCancel(event)
+  {
+    this.props.onCancel("new", {fname:"", lname:"", email:""});
+    event.preventDefault();
+  }
+
+  componentWillReceiveProps(newProps) {
+      if (newProps.author != null) {
+        this.setState({
+          fname: newProps.author.fname,
+          lname: newProps.author.lname,
+          email: newProps.author.email,
+          id: newProps.author.id,
+        });
+      }
+  }
+
+  renderButtons() {
+    if (this.props.formMode === "new") {
+      return(
+        <button type="submit" className="btn btn-primary">Create</button>
+      );
+    } else {
+      return(
+        <div className="form-group">
+          <button type="submit" className="btn btn-primary">Save</button>
+          <button type="submit" className="btn btn-danger" onClick={this.handleCancel} >Cancel</button>
+        </div>
+      );
+    }
+  }
+
+  render()  {
+    return (
+      <div className="author-form">
+        <h1> Authors </h1>
+        <form onSubmit={this.handleSubmit}>
+          <div className="form-group">
+            <label>First Name</label>
+            <input type="text" className="form-control" autoComplete='given-name' name="fname" id="fname" placeholder="First Name" value={this.state.fname} onChange={this.handleInputChange}/>
+          </div>
+          <div className="form-group">
+            <label htmlFor="lname">Last Name</label>
+            <input type="text" className="form-control" autoComplete='family-name' name="lname" id="lname" placeholder="Last Name" value={this.state.lname} onChange={this.handleInputChange}/>
+          </div>
+          <div className="form-group">
+            <label htmlFor="email">Email address</label>
+            <input type="email" className="form-control" autoComplete='email' name="email"  id="email" placeholder="name@example.com" value={this.state.email} onChange={this.handleInputChange}/>
+          </div>
+          {this.renderButtons()}
+        </form>
+      </div>
+    );
+  }
 }
 
 export default AuthorForm;
--- src/components/authors.js
+++ src/components/authors.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import AuthorForm from './author_form';
 import AuthorList from './author_list';
+import axios from 'axios';
+const API_BASE = "http://localhost:3000/";
 
 class Authors extends React.Component {
 
@@ -8,19 +10,86 @@ class Authors extends React.Component {
     super(props);
     this.state = {
       authors: [],
+      formMode: "new",
+      author: {lname:"", fname:"", email:""}
     };
     this.loadAuthors = this.loadAuthors.bind(this);
+    this.removeAuthor = this.removeAuthor.bind(this);
+    this.addAuthor = this.addAuthor.bind(this);
+    this.updateAuthor = this.updateAuthor.bind(this);
+  }
+
+  // loadAuthors() {
+  //   this.setState({
+  //     authors: [
+  //       {id: 1, fname: "sam", lname: "iam", email: "sam@aol.com"},
+  //       {id: 2, fname: "jane", lname: "doe", email: "jane@aol.com"},
+  //       {id: 3, fname: "fred", lname: "bear", email: "fred@aol.com"},
+  //       {id: 4, fname: "ted", lname: "tooy", email: "ted@aol.com"},
+  //     ]}
+  //   );
+  // }
+
+  updateForm(mode, authorVals) {
+    this.setState({
+      author: Object.assign({}, authorVals),
+      formMode: mode,
+    });
+  }
+
+  clearForm()
+  {
+    console.log("clear form");
+    this.updateForm("new",{fname:"",lname:"",email:""});
+  }
+
+  formSubmitted(author) {
+    if(this.state.formMode === "new") {
+      this.addAuthor(author);
+    } else {
+      this.updateAuthor(author);
+    }
+    this.clearForm();
   }
 
   loadAuthors() {
-    this.setState({
-      authors: [
-        {id: 1, fname: "sam", lname: "iam", email: "sam@aol.com"},
-        {id: 2, fname: "jane", lname: "doe", email: "jane@aol.com"},
-        {id: 3, fname: "fred", lname: "bear", email: "fred@aol.com"},
-        {id: 4, fname: "ted", lname: "tooy", email: "ted@aol.com"},
-      ]}
-    );
+    axios
+    .get(`${API_BASE}/authors`)
+    .then(res => {
+      this.setState({ authors: res.data });
+      console.log(`Data loaded! = ${this.state.authors}`)
+    })
+    .catch(err => console.log(err));
+  }
+
+  addAuthor(newAuthor) {
+    axios
+    .post(`${API_BASE}/authors`, newAuthor)
+    .then(res => {
+      res.data.key = res.data.id;
+      this.setState({ authors: [...this.state.authors, res.data] });
+    })
+    .catch(err => console.log(err));
+  }
+
+  updateAuthor(author) {
+    axios
+    .put(`${API_BASE}/authors/${author.id}`, author)
+    .then(res => {
+      this.loadAuthors();
+    })
+    .catch(err => console.log(err));
+  }
+
+  removeAuthor(id) {
+    let filteredArray = this.state.authors.filter(item => item.id !== id)
+    this.setState({authors: filteredArray});
+    axios
+    .delete(`${API_BASE}/authors/${id}`)
+    .then(res => {
+      console.log(`Record Deleted`);
+    })
+    .catch(err => console.log(err));
   }
 
   componentDidMount() {
@@ -31,8 +100,17 @@ class Authors extends React.Component {
   render() {
     return (
       <div className="authors">
-        <AuthorForm />
-        <AuthorList authors={this.state.authors}/>
+        <AuthorForm
+          onSubmit={(author) => this.formSubmitted(author)}
+          onCancel={(mode,author) => this.updateForm(mode,author)}
+          formMode={this.state.formMode}
+          author={this.state.author}
+        />
+        <AuthorList
+          authors={this.state.authors}
+          onDelete={(id) => this.removeAuthor(id)}
+          onEdit={(mode,author) => this.updateForm(mode,author)}
+        />
       </div>
     );
   }
